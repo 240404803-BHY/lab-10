798939fb710b120721fff9b9159c905153e534a3
Berk Hakan Yılmaz <240404803@st.biruni.edu.tr>
2025-12-09 22:59:31 +0300

    chore: özel fontlarla ilk proje kurulumu

---
+++ App.js
@@ -0,0 +1,15 @@
+import { useFonts } from 'expo-font';
+import ProfileScreen from './screens/ProfileScreen';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Montserrat: require('./assets/fonts/Montserrat-Regular.ttf'),
+    MontserratBold: require('./assets/fonts/Montserrat-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+
+    return null;
+  }
+  return <ProfileScreen />;
+}
